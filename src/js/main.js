9e8a322394567890e871789d7d56366a093565d6
Eremeev Ivan <stee15454@yandex.ru>
2019-02-05 18:37:13 +0500

    Добавил секцию 'О Нас' и якорь к ней

--- src/js/main.js
+++ src/js/main.js
@@ -80,4 +80,46 @@ document.addEventListener('DOMContentLoaded', function () {
             overlayMenu.style.backgroundColor='transparent';
         }
     });
+
+
+    var linkNav = document.querySelectorAll('[href^="#"]'), //выбираем все ссылки к якорю на странице
+        V = 1;  // скорость, может иметь дробное значение через точку (чем меньше значение - тем больше скорость)
+    for (var i = 0; i < linkNav.length; i++) {
+        linkNav[i].addEventListener('click', function(e) { //по клику на ссылку
+            e.preventDefault(); //отменяем стандартное поведение
+            var w = window.pageYOffset,  // производим прокрутка прокрутка
+                hash = this.href.replace(/[^#]*(.*)/, '$1');  // к id элемента, к которому нужно перейти
+            t = document.querySelector(hash).getBoundingClientRect().top,  // отступ от окна браузера до id
+                start = null;
+            requestAnimationFrame(step);  // подробнее про функцию анимации [developer.mozilla.org]
+            function step(time) {
+                if (start === null) start = time;
+                var progress = time - start,
+                    r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
+                window.scrollTo(0,r);
+                if (r != w + t) {
+                    requestAnimationFrame(step)
+
+                                                            // Скрытие меню после нажатия
+
+                    if (menuToggle.className != 'main-nav__toggle' ) {
+                        menuToggle.classList.remove('main-nav__toggle-closed');
+                        menuShedow.style.boxShadow='0 0 0';
+                        menuMain.style.flexDirection='row';
+                        userListHidden.style.display='none';
+                        siteListHidden.style.display='none';
+                        overlayMenu.style.position='absolute';
+                        overlayMenu.style.height='auto';
+                        overlayMenu.style.backgroundColor='transparent';
+                    } else {
+                        return
+                    }
+
+
+                } else {
+                    location.hash = hash  // URL с хэшем
+                }
+            }
+        }, false);
+    }
 });
